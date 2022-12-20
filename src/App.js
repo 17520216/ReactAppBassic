08877119ce78afbcdb604e9649fa8bb744ba3b83
annguyen1511 <annguyen1511>
2022-12-20 20:57:17 +0700

    change title project

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="main">
       <Header />
       <main>
-        <h1>My Shop</h1>
+        <h1>My Project</h1>
         <div className="content">
           <Products name="Shoes" price="10$" type="green" images="https://media.istockphoto.com/id/1341840659/photo/defocused-photo-of-police-car-lights-in-night-city-with-selective-focus-and-bokeh.jpg?b=1&s=170667a&w=0&k=20&c=bMyrFz2WOkjHx-86mDElvLmC2FSldPTiO52oHzTwTPE=" />
           <Products name="Phone" price="7$" type="red" images="https://media.istockphoto.com/id/559537031/photo/lens-flash-of-paparazzi-photographers-cameras.jpg?b=1&s=170667a&w=0&k=20&c=XozHRUg50PRcnP96qM3OK8RFZNNSsgG3G78tIjf8bXI=" />
